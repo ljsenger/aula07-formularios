c2ac76aac309a478b11e933c09c54e2f5098116b
Luciano Senger <ljsenger@gmail.com>
2019-10-15 00:02:15 -0300

    personalizar mensagem de erro

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
 	usuario = StringField(u'Nome de usuário', validators=[DataRequired()])
-	senha = PasswordField(u'Senha', validators=[DataRequired()])
+	senha = PasswordField(u'Senha', validators=[DataRequired(message="Campo não deve ser vazio")])
 	lembre_me = BooleanField(u'Lembre-me')
 	submit = SubmitField(u'Entrar')
